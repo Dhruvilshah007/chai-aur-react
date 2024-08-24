5ec2704e4322ebd785e2e4832a0a546421182a3d
Dhruvilshah007 <sdhruvil72@gmail.com>
2024-08-24 23:45:17 +0530

    Password generator-useEffect,useCallback,useRef

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,113 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useRef hook
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  //setPassword is added because for optimization, if we have added password it will go in infonte loop, it is alse fine if we dont give setPassword, it is just for optimization
+  //Including setPassword in the dependency array is primarily a safeguard to ensure that the function remains consistent with the latest state. It's a best practice to include all dependencies that are used within the function to prevent potential issues in more complex applications.
+
+  // passwordGenerator(); we cannot call it here directly,too many re-renders
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+
+    //We can keep range if require
+    passwordRef.current?.setSelectionRange(0, 3);
+    //here in react it will run in browser so we have access of window,but same is not accesble in next.js
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  return (
+    <>
+      <h1 className="text-4xl text-center text-white my-3">
+        Password Generator
+      </h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500">
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className="outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0 rounded-none"
+          >
+            copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="charInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="charInput">Characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+//useCallback - useCallback is a React Hook that lets you cache a function definition between re-renders.It takes function and dependencies as here we jave number and character dependencies,on which function should be called
